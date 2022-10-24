8407bdd3eda1332515da997708482fb7a67b7ae2
mariaka86 <marymwihaki123@gmail.com>
2022-10-24 18:29:43 -0500

    finished challenge 26

---
+++ javascript/401-code-challenges/array-insert-sort/array-insert-sort.js
@@ -0,0 +1,12 @@
+function insertionSort(arr) {
+  for (let i = 1 < arr.length; i++;) {
+    let j = i - 1;
+    let temp = arr[i];
+    while (j >= 0 && temp < arr[j]){
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = temp;
+  }
+}
+return insertionSort;
